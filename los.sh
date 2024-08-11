1e109e8923e667143166124ebbdb73a8c0e00951
aghora7 <ukikieran@gmail.com>
2024-08-11 10:40:03 +0530

    oneplus : Lineage repo clone

---
+++ los.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/MT6983-Dev/android_device_oneplus_vitamin device/oneplus/vitamin
+git clone https://github.com/MT6983-Dev/android_device_oneplus_vitamin-kernel device/oneplus/vitamin-kernel
+git clone https://gitlab.com/Aghora7/android_vendor_oneplus_vitamin vendor/oneplus/vitamin
+git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus
+git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
