16a7140725285d7e5024b5603c11626f249b58c6
aghora7 <ukikieran@gmail.com>
2024-09-07 10:25:39 +0530

    update kernel trees

--- los.sh
+++ los.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/MT6983-Dev/android_device_oneplus_vitamin device/oneplus/vitamin
 git clone https://github.com/MT6983-Dev/android_device_oneplus_vitamin-kernel device/oneplus/vitamin-kernel
-git clone https://gitlab.com/Aghora7/android_vendor_oneplus_vitamin vendor/oneplus/vitamin
+git clone https://gitlab.com/Aghora7/android_vendor_oneplus_vitamin vendor/oneplus/vitamin --depth=1
 git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus
 git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 git clone https://github.com/MT6983-Dev/android_hardware_oplus hardware/oplus
+git clone https://github.com/PQEnablers-Devices/android_kernel_mediatek_5.10 kernel/mediatek/k510 --depth=1
