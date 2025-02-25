869e207a8a37e32bfd66f08de669aeed103dfd1f
Aghora7 <ukikieran@gmail.com>
2025-02-25 21:08:20 +0530

    Create lov.sh
    clone from lineageos-on-vitamin

---
+++ lov.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/lineageos-on-vitamin/android_device_oneplus_vitamin device/oneplus/vitamin -b lineage-22.1
+git clone https://github.com/lineageos-on-vitamin/android_device_oneplus_vitamin-kernel device/oneplus/vitamin-kernel -b lineage-22.1
+git clone https://gitlab.com/Aghora7/android_vendor_oneplus_vitamin vendor/oneplus/vitamin --depth=1 -b lineage-22.1
+git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus -b lineage-22.1
+git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek -b lineage-22.1
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-22.1
+git clone https://github.com/lineageos-on-vitamin/android_hardware_oplus hardware/oplus -b lineage-22.1
+git clone https://github.com/PQEnablers-Devices/android_kernel_mediatek_5.10 kernel/mediatek/k510 --depth=1
